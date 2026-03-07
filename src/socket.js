f9c391b4e4fb4eaded989e369f5e557206bc5087
Anuj <anuj2000kashyap@gmail.com>
2026-03-08 03:19:13 +0530

    Changes the url of socket

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // Connect to the BACKEND port (8080)
 // The "origin" in your server code refers to THIS app (3000)
-const socket = io("http://localhost:8080");
+const socket = io("https://hirelingbackend.onrender.com");
 
 socket.on("connect", () => {
   console.log("Connected to Backend! ID:", socket.id);
