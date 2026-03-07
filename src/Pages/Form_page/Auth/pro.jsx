d2acb062c0011efdc657b2a22b58fdcc677bcb22
Anuj <anuj2000kashyap@gmail.com>
2026-03-08 02:49:45 +0530

    case sensitive

--- src/Pages/Form_page/Auth/pro.jsx
+++ src/Pages/Form_page/Auth/pro.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Pro.css"; 
+import "./pro.css"; 
 import logo from "../../../assets/logo.png";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
